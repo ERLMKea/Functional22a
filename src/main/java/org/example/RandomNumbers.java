9350ba289311406b19f8b8ce85089511a30cd85c
erlm <erlm@kea.dk>
2023-10-30 13:20:35 +0100

    more comments

--- src/main/java/org/example/RandomNumbers.java
+++ src/main/java/org/example/RandomNumbers.java
@@ -8,13 +8,19 @@ import java.util.stream.Stream;
 public class RandomNumbers {
 
     public static void main(String[] args) {
-        Supplier<Double> ran = () -> Math.random();
-        Stream<Double> randomNumbers = Stream.generate(ran);
-        Function<Double, Long> dblround = d -> Math.round(d*100);
-        Predicate<Long> longEqual = rannum -> rannum % 2 == 0;
-        double sumd = randomNumbers.map(dblround).filter(longEqual)
-                .limit(12).mapToDouble(d->d).sum();
+        Supplier<Double> ran = () -> Math.random(); //generate random numbers between 0 and 1
+        Stream<Double> randomNumbers = Stream.generate(ran); //generate forever
+        Function<Double, Long> dblround = d -> Math.round(d*100); //convert numbers 0-1 to 1-100
+        Predicate<Long> longEqual = rannum -> rannum % 2 == 0; //we only want even numbers
+        double sumd = randomNumbers.map(dblround).filter(longEqual) //get even numbers
+                .limit(12).mapToDouble(d->d).sum(); //limit forever to 12 numbers, and then sum. could also call e.g. average() or max()
         System.out.println("sum af 12numre=" + sumd);
+
+        Stream<Double> randomNumbers2 = Stream.generate(ran); //generate forever
+        var optD = randomNumbers2.map(dblround).filter(longEqual) //get even numbers
+                .limit(12).mapToDouble(d->d).average();
+        System.out.println(optD.getAsDouble());
+
 
         //obj.forEach(System.out::println);
         //Stream<Long> num100 = randomNumbers.map(dblround);
